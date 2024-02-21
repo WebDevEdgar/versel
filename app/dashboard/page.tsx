b9bbfef36b92927a9ca9809010521b42d286c66e
Edgar <khachaturovedgar01@gmail.com>
2024-02-21 22:50:02 +0400

    added default

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { lusitana } from '@/app/ui/fonts';
 
-export module Page{
+export default function Page(){
     return (
         <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
